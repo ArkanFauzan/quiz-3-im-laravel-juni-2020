994df6306ccf27c33c0bc6b50d08a8a92617f0a9
ArkanFauzan <arkanfauzan93@gmail.com>
2020-07-05 14:22:33 +0700

    Quiz CRUD Laravel

---
+++ database/migrations/2020_07_05_035427_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('name',45);
+            $table->string('password',45);
+            $table->string('bio',45)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
